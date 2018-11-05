1f059569564f4de634e94b141af7adbfeafee34c
Marcel Keller <mks.keller@gmail.com>
2018-11-05 16:11:08 +1100

    Minor.

--- Scripts/setup-ssl.sh
+++ Scripts/setup-ssl.sh
@@ -4,6 +4,8 @@ n=${1:-3}
 
 echo Setting up SSL for $n parties
 
+mkdir Player-Data
+
 for i in `seq 0 $[n-1]`; do
     openssl req  -nodes -x509 -out Player-Data/P$i.pem -keyout Player-Data/P$i.key -subj "/CN=P$i"
 done
