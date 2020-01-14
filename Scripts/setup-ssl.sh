f5130bee17f942d9915a440eb071cb6194a63a6e
Mark Abspoel <mail@markabspoel.nl>
2020-01-14 10:17:01 +0100

    Use Homebrew-installed OpenSSL on macOS for setup-ssl.sh

--- Scripts/setup-ssl.sh
+++ Scripts/setup-ssl.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# brew-installed OpenSSL on MacOS
+PATH=/usr/local/opt/openssl/bin:$PATH
+
 n=${1:-3}
 
 test -e Player-Data || mkdir Player-Data
@@ -10,6 +13,4 @@ for i in `seq 0 $[n-1]`; do
     openssl req -newkey rsa -nodes -x509 -out Player-Data/P$i.pem -keyout Player-Data/P$i.key -subj "/CN=P$i"
 done
 
-# brew-installed OpenSSL on MacOS
-PATH=$PATH:/usr/local/opt/openssl/bin/
 c_rehash Player-Data
