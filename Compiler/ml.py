a94c2d7613c0307f3c5b913434bfbb246edca91d
Marcel Keller <mks.keller@gmail.com>
2021-07-09 20:11:45 +1000

    Missing code for square activation.

--- Compiler/ml.py
+++ Compiler/ml.py
@@ -910,6 +910,12 @@ class ElementWiseLayer(NoVariableLayer):
         self.nabla_Y = Tensor(backward_shape, sfix)
         self.inputs = inputs
 
+    def f_part(self, base, size):
+        return self.f(self.X.get_part_vector(base, size))
+
+    def f_prime_part(self, base, size):
+        return self.f_prime(self.Y.get_part_vector(base, size))
+
     def _forward(self, batch=[0]):
         n_per_item = reduce(operator.mul, self.X.sizes[1:])
         @multithread(self.n_threads, len(batch), max(1, 1000 // n_per_item))
