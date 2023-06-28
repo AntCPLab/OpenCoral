badf6dc4ffd97f1fbe4e063c483b732c3090873b
Zhicong Huang <zhicong303@gmail.com>
2023-06-28 15:48:02 +0800

    type conv between ntl and mp spdz

---
+++ Tools/mpdz_ntl_types.h
@@ -0,0 +1,31 @@
+
+#include "Math/gf2n.h"
+#include "Math/mfe.h"
+
+void ntl_gf2x_to_mpdz_gf2n(gf2n_short& y, const NTL::GF2X& x) {
+    if (deg(x) >= gf2n_short::degree())
+        throw runtime_error("Cannot convert NTL::GF2X to a smaller gf2n element");
+    y = gf2n_short(x.xrep[0]);
+}
+
+gf2n_short ntl_gf2x_to_mpdz_gf2n(const NTL::GF2X& x) {
+    gf2n_short y;
+    ntl_gf2x_to_mpdz_gf2n(y, x);
+    return y;
+}
+
+void mpdz_gf2n_to_ntl_gf2x(NTL::GF2X& y, const gf2n_short& x) {
+    if (x.get_word() == 0) {
+        clear(y);
+    }
+    else {
+        y.xrep.SetLength(1);
+        y.xrep[0] = x.get_word();
+    }
+}
+
+NTL::GF2X mpdz_gf2n_to_ntl_gf2x(const gf2n_short& x) {
+    NTL::GF2X y;
+    mpdz_gf2n_to_ntl_gf2x(y, x);
+    return y;
+}
