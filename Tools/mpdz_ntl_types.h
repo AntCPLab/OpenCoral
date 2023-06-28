32ee18129b42a88c7764a65ec895003f1c28df51
Zhicong Huang <zhicong303@gmail.com>
2023-06-28 16:04:07 +0800

    conv api

--- Tools/mpdz_ntl_types.h
+++ Tools/mpdz_ntl_types.h
@@ -2,6 +2,10 @@
 #include "Math/gf2n.h"
 #include "Math/mfe.h"
 
+void conv(gf2n_short& y, const NTL::GF2X& x) {
+    ntl_gf2x_to_mpdz_gf2n(y, x);
+}
+
 void ntl_gf2x_to_mpdz_gf2n(gf2n_short& y, const NTL::GF2X& x) {
     if (deg(x) >= gf2n_short::degree())
         throw runtime_error("Cannot convert NTL::GF2X to a smaller gf2n element");
@@ -12,6 +16,10 @@ gf2n_short ntl_gf2x_to_mpdz_gf2n(const NTL::GF2X& x) {
     gf2n_short y;
     ntl_gf2x_to_mpdz_gf2n(y, x);
     return y;
+}
+
+void conv(NTL::GF2X& y, const gf2n_short& x) {
+    mpdz_gf2n_to_ntl_gf2x(y, x);
 }
 
 void mpdz_gf2n_to_ntl_gf2x(NTL::GF2X& y, const gf2n_short& x) {
