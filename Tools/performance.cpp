52dfc96ac2c2b2f74dd18955947d9171d82cac8c
zicofish <zhicong303@gmail.com>
2023-10-14 21:49:49 +0800

    update perf

--- Tools/performance.cpp
+++ Tools/performance.cpp
@@ -1,3 +1,4 @@
 #include "performance.h"
 
 std::map<std::string, std::chrono::nanoseconds> profiling;
+GlobalPerformance* GlobalPerformance::singleton = 0;
--- Tools/performance.h
+++ Tools/performance.h
@@ -8,6 +8,7 @@
 #include <utility>
 #include <chrono>
 #include <map>
+#include <mutex>
 
 
 inline std::chrono::nanoseconds time_log(const std::string& tag, bool print=true) {
@@ -52,5 +53,82 @@ inline void print_profiling(const std::string& tag="") {
 inline double get_acc_time_log(const std::string& tag) {
     return std::chrono::duration_cast<std::chrono::milliseconds>(profiling[tag]).count() * 1.0;
 }
+
+class ThreadPerformance {
+public:
+    std::string tag;
+    std::chrono::high_resolution_clock::time_point start_time;
+    std::chrono::nanoseconds duration;
+    size_t start_comm;
+    size_t total_comm;
+
+    ThreadPerformance(const std::string& tag, size_t start_comm = 0): tag(tag), start_comm(start_comm) {
+        start(start_comm);
+    }
+
+    void start(size_t start_comm = 0) {
+        start_time = std::chrono::high_resolution_clock::now();
+    }
+
+    void stop(size_t end_comm = 0) {
+        auto end = std::chrono::high_resolution_clock::now();
+        duration = std::chrono::duration_cast<std::chrono::nanoseconds>(end-start_time);
+        total_comm = end_comm - start_comm;
+    }
+};
+
+class GlobalPerformance {
+public:
+    static GlobalPerformance* singleton;
+
+    std::map<std::string, std::chrono::nanoseconds> logs;
+    std::map<std::string, size_t> comm_logs;
+    std::mutex mtx;
+
+    static GlobalPerformance& s() {
+        if (singleton)
+            return *singleton;
+        else
+            throw std::runtime_error("no singleton: GlobalPerformance");
+    }
+
+    GlobalPerformance() {
+        if (singleton)
+            throw std::runtime_error("there can only be one GlobalPerformance");
+        singleton = this;
+    }
+
+    void add(const ThreadPerformance& log) {
+        mtx.lock();
+        logs[log.tag] += log.duration;
+        comm_logs[log.tag] += log.total_comm;
+        mtx.unlock();
+    }
+
+    void print_time(const std::string& tag="") {
+        if (tag == "") {
+            for(const auto& e : profiling)
+                print_time(e.first);
+        }
+        else {
+            std::cout << "[Total Time] " << tag << ": " 
+                << std::chrono::duration_cast<std::chrono::milliseconds>(logs[tag]).count() * 1.0
+                << " ms" << std::endl;
+        }
+    }
+
+    void print_comm(const std::string& tag="") {
+        if (tag == "") {
+            for(const auto& e : profiling)
+                print_comm(e.first);
+        }
+        else {
+            std::cout << "[Total Comm] " << tag << ": " 
+                << comm_logs[tag] * 1.0 / 1e6
+                << " MB" << std::endl;
+        }
+    }
+
+};
 
 #endif // PERFORMANCE_H
