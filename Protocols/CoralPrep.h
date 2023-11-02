a6e3f52d2898757da81c5aa10638efb66721cd4d
zicofish <zhicong303@gmail.com>
2023-11-02 18:03:37 +0800

    minor

--- Protocols/CoralPrep.h
+++ Protocols/CoralPrep.h
@@ -3,8 +3,8 @@
  *
  */
 
-#ifndef PROTOCOLS_CoralPREP_H_
-#define PROTOCOLS_CoralPREP_H_
+#ifndef PROTOCOLS_CORALPREP_H_
+#define PROTOCOLS_CORALPREP_H_
 
 #include "CoralShare.h"
 #include "Spdz2kPrep.h"
@@ -17,13 +17,6 @@
 template<class T>
 class CoralPrep : public virtual Spdz2kPrep<T>
 {
-    // typedef typename T::bit_prep_type BitShare;
-    // DataPositions bit_pos;
-    // MascotTriplePrep<BitShare>* bit_prep;
-    // SubProcessor<BitShare>* bit_proc;
-    // typename BitShare::MAC_Check* bit_MC;
-
-    RmfeShareConverter<GC::Spdz2kBShare<T::s>>* spdz2k2rmfe;
 
 public:
     CoralPrep(SubProcessor<T>* proc, DataPositions& usage);
@@ -35,4 +28,4 @@ public:
     void buffer_dabits(ThreadQueues* queues = 0);
 };
 
-#endif /* PROTOCOLS_SPDZ2KPREP_H_ */
+#endif /* PROTOCOLS_CORALPREP_H_ */
