880f106dd9c220983e7f86835b9af71297895062
zicofish <zhicong303@gmail.com>
2023-11-03 16:13:59 +0800

    update wan

--- Scripts/throttle.sh
+++ Scripts/throttle.sh
@@ -14,6 +14,6 @@ fi
 if [ "$1" == "wan" ]
 then
 sudo tc qdisc del dev $DEV root
-sudo tc qdisc add dev $DEV root handle 1: tbf rate 500mbit burst 100000 limit 10000
+sudo tc qdisc add dev $DEV root handle 1: tbf rate 200mbit burst 100000 limit 10000
 sudo tc qdisc add dev $DEV parent 1:1 handle 10: netem delay 10msec
 fi
